3460794e6b4341b69fbbdcd75a063db0ee285864
Jason Knott <24280037+jayknott@users.noreply.github.com>
2020-12-07 10:58:01 -0800

    update

--- src/simple-flexbox-card.ts
+++ src/simple-flexbox-card.ts
@@ -1,97 +1,53 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import {
-  LitElement,
-  html,
-  customElement,
-  property,
-  CSSResult,
-  TemplateResult,
-  css,
-  PropertyValues,
-  internalProperty,
-} from 'lit-element';
 import {
   HomeAssistant,
-  hasConfigOrEntityChanged,
   LovelaceCard,
-  getLovelace,
   LovelaceCardConfig,
-} from 'custom-card-helpers'; // This is a community maintained npm module with common helper functions/types
-
+} from 'custom-card-helpers';
+import {
+  LitElement as LitElementSource,
+  html as htmlSource,
+  css as cssSource,
+  internalProperty,
+  property,
+} from 'lit-element';
 import type { SimpleFlexboxCardConfig } from './types';
 import { CARD_VERSION } from './const';
 import { localize } from './localize/localize';
 
-const cardHelpers = (window as any).loadCardHelpers()
-  ? (window as any).loadCardHelpers()
-  : undefined;
+const bases = [
+  customElements.whenDefined('hui-masonry-view'),
+  customElements.whenDefined('hc-lovelace'),
+];
+Promise.race(bases).then(() => {
+  const LitElement: typeof LitElementSource = customElements.get(
+    'hui-masonry-view',
+  )
+    ? Object.getPrototypeOf(customElements.get('hui-masonry-view'))
+    : Object.getPrototypeOf(customElements.get('hc-lovelace'));
 
-/* eslint no-console: 0 */
-console.info(
-  `%c  SIMPLE-FLEXBOX-CARD  \n%c  ${localize(
-    'common.version',
-  )} ${CARD_VERSION}        `,
-  'color: orange; font-weight: bold; background: black',
-  'color: white; font-weight: bold; background: dimgray',
-);
+  const html: typeof htmlSource = (LitElement.prototype as any).html;
 
-// This puts your card into the UI card picker dialog
-(window as any).customCards = (window as any).customCards || [];
-(window as any).customCards.push({
-  type: 'simple-flexbox-card',
-  name: 'Simple Flexbox Card',
-  description: 'Display a simple flexbox in Lovelace dashboards.',
-});
+  const css: typeof cssSource = (LitElement.prototype as any).css;
 
-@customElement('simple-flexbox-card')
-export class SimpleFlexboxCard extends LitElement {
-  public static getStubConfig(): object {
-    return {};
-  }
-
-  // TODO Add any properties that should cause your element to re-render here
-  // https://lit-element.polymer-project.org/guide/properties
-  @property({ attribute: false }) public hass!: HomeAssistant;
-  @internalProperty() private config!: SimpleFlexboxCardConfig;
-  @internalProperty() private _cards: Array<LovelaceCardConfig> = [];
-  @internalProperty() private _refCards: Array<LovelaceCard> = [];
-
-  // https://lit-element.polymer-project.org/guide/properties#accessors-custom
-  public async setConfig(config: SimpleFlexboxCardConfig): Promise<void> {
-    // TODO Check for required fields and that they are of the proper format
-    if (!config) {
-      throw new Error(localize('common.invalid_configuration'));
-    }
-
-    if (config.test_gui) {
-      getLovelace().setEditMode(true);
-    }
-
-    this.config = config;
-    if (config.cards) this._cards = config.cards;
-
-    if (this.hass) this.renderCard();
-  }
-
-  renderCard() {
-    const promises = this._cards.map((card) => this.createCardElement(card));
-    Promise.all(promises).then((card_objects) => {
-      this._refCards = card_objects;
-    });
-  }
-
-  createError(error: Error, config: LovelaceCardConfig): Promise<LovelaceCard> {
-    return this.createElement('hui-error-card', {
+  const createError = (
+    error: Error,
+    config: LovelaceCardConfig,
+  ): Promise<LovelaceCard> => {
+    return createCardFromHelper('hui-error-card', {
       type: 'error',
       error,
       config,
     });
-  }
+  };
 
-  async createElement(
+  const cardHelpers = (window as any).loadCardHelpers()
+    ? (window as any).loadCardHelpers()
+    : undefined;
+
+  const createCardFromHelper = async (
     tag: string,
     config: LovelaceCardConfig,
-  ): Promise<LovelaceCard> {
+  ): Promise<LovelaceCard> => {
     if (cardHelpers) {
       const cardHelper = await cardHelpers;
       return cardHelper.createCardElement(config);
@@ -103,80 +59,128 @@ export class SimpleFlexboxCard extends LitElement {
       element.setConfig(config);
     } catch (err) {
       console.error(tag, err);
-      return this.createError(err.message, config);
+      return await createError(err.message, config);
     }
-
-    console.log(element);
 
     return element;
-  }
+  };
 
-  async createCardElement(
-    cardConfig: LovelaceCardConfig,
-  ): Promise<LovelaceCard> {
-    let tag = cardConfig.type;
+  class SimpleFlexboxCard extends LitElement {
+    @property({ attribute: false }) public _hass!: HomeAssistant;
+    @internalProperty() private _config!: SimpleFlexboxCardConfig;
+    @internalProperty() private _cards: Array<LovelaceCardConfig> = [];
+    @internalProperty() private _refCards?: Array<LovelaceCard>;
 
-    if (tag.startsWith('divider')) {
-      tag = `hui-divider-row`;
-    } else if (tag.startsWith('custom:')) {
-      tag = tag.substr('custom:'.length);
-    } else {
-      tag = `hui-${tag}-card`;
+    public static getStubConfig(): object {
+      return {};
     }
 
-    const element = await this.createElement(tag, cardConfig);
-
-    element.hass = this.hass;
-
-    element.addEventListener(
-      'll-rebuild',
-      (ev) => {
-        ev.stopPropagation();
-        this.createCardElement(cardConfig).then(() => {
-          this.renderCard();
-        });
-      },
-      { once: true },
-    );
-
-    return element;
-  }
-
-  // https://lit-element.polymer-project.org/guide/lifecycle#shouldupdate
-  protected shouldUpdate(changedProps: PropertyValues): boolean {
-    if (!this.config) {
-      return false;
-    }
-
-    return hasConfigOrEntityChanged(this, changedProps, false);
-  }
-
-  // https://lit-element.polymer-project.org/guide/templates
-  protected render(): TemplateResult | void {
-    const items = this._refCards.map((card) => html`<li>${card}</li>`);
-
-    return html`<ul class="flex-container">
-      ${items}
-    </ul>`;
-  }
-
-  // https://lit-element.polymer-project.org/guide/styles
-  static get styles(): CSSResult {
-    return css`
-      .flex-container {
-        padding: 0;
-        margin: 0 0 0 0;
-        list-style: none;
-        -ms-box-orient: horizontal;
-        display: -webkit-box;
-        display: -moz-box;
-        display: -ms-flexbox;
-        display: -moz-flex;
-        display: -webkit-flex;
-        display: flex;
-        -webkit-flex-wrap: wrap;
-        flex-wrap: wrap;
+    set hass(hass: HomeAssistant) {
+      this._hass = hass;
+      if (!this._refCards && this._config) {
+        this.renderCard();
+        return;
       }
-    `;
+      if (this._refCards) {
+        this._refCards.forEach((card) => {
+          card.hass = hass;
+        });
+      }
+    }
+
+    setConfig(config: SimpleFlexboxCardConfig): void {
+      if (!config?.cards || !Array.isArray(config.cards)) {
+        throw new Error('Card config incorrect');
+      }
+
+      this._config = config;
+      this._cards = config.cards;
+
+      if (this._hass) this.renderCard();
+    }
+
+    renderCard(): void {
+      const promises = this._cards.map((config, config_i) =>
+        this.createCardElement(config, config_i),
+      );
+      Promise.all(promises).then((cards) => (this._refCards = cards));
+    }
+
+    async createCardElement(
+      cardConfig: LovelaceCardConfig,
+      index: number,
+    ): Promise<LovelaceCard> {
+      let tag = cardConfig.type;
+
+      if (tag.startsWith('divider')) {
+        tag = `hui-divider-row`;
+      } else if (tag.startsWith('custom:')) {
+        tag = tag.substr('custom:'.length);
+      } else {
+        tag = `hui-${tag}-card`;
+      }
+
+      const element = await createCardFromHelper(tag, cardConfig);
+
+      element.hass = this._hass;
+
+      element.addEventListener(
+        'll-rebuild',
+        (ev) => {
+          console.log(`ll-rebuild ${index}`);
+          ev.stopPropagation();
+          this.createCardElement(cardConfig, index).then((card) => {
+            (this._refCards as Array<LovelaceCard>)[index] = card;
+          });
+        },
+        { once: true },
+      );
+
+      return element;
+    }
+
+    render() {
+      if (!this._config || !this._hass || !this._refCards) {
+        return html``;
+      }
+
+      return html`<ul class="flex-container">
+        ${this._refCards.map((card) => html`<li>${card}</li>`)}
+      </ul>`;
+    }
+
+    static get styles() {
+      return css`
+        .flex-container {
+          padding: 0;
+          margin: 0 0 0 0;
+          list-style: none;
+          -ms-box-orient: horizontal;
+          display: -webkit-box;
+          display: -moz-box;
+          display: -ms-flexbox;
+          display: -moz-flex;
+          display: -webkit-flex;
+          display: flex;
+          -webkit-flex-wrap: wrap;
+          flex-wrap: wrap;
+        }
+      `;
+    }
+
+    getCardSize() {
+      return 3;
+    }
   }
-}
+
+  if (!customElements.get('simple-flexbox-card')) {
+    customElements.define('simple-flexbox-card', SimpleFlexboxCard as any);
+    console.info(
+      `%c  SIMPLE-FLEXBOX-CARD  \n%c  ${localize(
+        'common.version',
+      )} ${CARD_VERSION}        `,
+      'color: orange; font-weight: bold; background: black',
+      'color: white; font-weight: bold; background: dimgray',
+    );
+  }
+});
